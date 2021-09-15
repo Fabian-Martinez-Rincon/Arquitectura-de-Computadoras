4e06381434c7f9393b6c709a9b5091921c2b67fa
Fabo xw <fabianmartinezrincon.123@gmail.com>
2021-09-15 09:11:15 -0300

    9.a

--- Practicas/Practica_1/9.A.asm
+++ Practicas/Practica_1/9.A.asm
@@ -2,15 +2,16 @@
 ; Devuelve el resultado también en AH
 ORG 3000H
  ROTARIZQ: ADD AH, AH
-  ADC AH, 0
+  ADC AH, 0 ;Suma el carry
   RET
 ORG 1000H
- b DB 27H ; (00100111) en binario
+
+ b DB 80h ; (10000000) en binario
+ 
 ORG 2000H
  MOV AH, b ; AH = 00100111
  ; Realizamos una rotación
  CALL ROTARIZQ ; AH = 01001110
- ; Realizamos una segunda rotación
- CALL ROTARIZQ ; AH = 10011100
+ 
 HLT
 END
--- Practicas/Practica_1/9.B.asm
+++ Practicas/Practica_1/9.B.asm
@@ -1,6 +1,9 @@
 ; Recibe el caracter a rotar en AH
-; Recibe la cantidad de posiciones en BH
 ; Devuelve el resultado también en AH
+ORG 3000H
+ ROTARIZQ: ADD AH, AH
+  ADC AH, 0 ;Suma el carry
+  RET
 
 ORG 4000H
  ROTARIZQ_N: CMP BH, 0 ; mientras BH>0
@@ -10,12 +13,23 @@ ORG 4000H
  JMP ROTARIZQ_N ; aprovecho la etiqueta de la subrutina
 ; para hacer el salto
 FIN: RET
+
+
 ORG 1000H
- b DB 27H ; (00100111) en binario
+
+ b DB 80h ; (10000000) en binario
+ 
 ORG 2000H
- MOV AH, b
- ; Realizamos una rotación de 2 posiciones a la izquierda
- MOV BH, 2
- CALL ROTARIZQ_N ; AH = 10011100 (C9H)
- HLT
-END 
+ MOV AH, b ; AH = 00100111
+ ; Realizamos una rotación
+ MOV BH,5
+ CALL ROTARIZQ_N ; AH = 01001110
+ 
+HLT
+END
+
+; Recibe el caracter a rotar en AH
+; Recibe la cantidad de posiciones en BH
+; Devuelve el resultado también en AH
+
+
