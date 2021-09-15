07890ae4b1bdc94d4b42cb62467df0365acb1714
Fabo xw <fabianmartinezrincon.123@gmail.com>
2021-09-15 09:20:18 -0300

    9.C

--- Practicas/Practica_1/9.C.asm
+++ Practicas/Practica_1/9.C.asm
@@ -1,19 +1,38 @@
-; Utiliza los mismos registros que ROTARIZQ_N
-; Recibe en BH la cantidad de posiciones
+; Recibe el caracter a rotar en AH
+; Devuelve el resultado también en AH
+
+
+ORG 3000H
+ ROTARIZQ: ADD AH, AH
+  ADC AH, 0 ;Suma el carry
+  RET
+
 ORG 5000H
 ROTARDER_N: MOV CH, 8
- SUB CH, BH ; cantidad de bytes que debo rotar hacia la izq.
- MOV BH, CH ; vuelvo a copiar en BH
+ SUB CH, BH ; cantidad de bytes que debo rotar hacia la izq. (8-N)
+ MOV BH, CH ; vuelvo a copiar en BH (8-N)
  ; ROTARIZQ usará el valor almacenado en BH para rotar.
  CALL ROTARIZQ_N
  RET
+
+ORG 4000H
+ ROTARIZQ_N: CMP BH, 0 ; mientras BH>0
+ JZ FIN ; si BH=0, entonces finalizar la subrut.
+ CALL ROTARIZQ
+ DEC BH
+ JMP ROTARIZQ_N ; aprovecho la etiqueta de la subrutina
+; para hacer el salto
+FIN: RET
+
 ORG 1000H
-b DB 27h ; (00100111) en binario
+ b DB 80h ; (10000000) en binario
  
 ORG 2000H
-MOV AH, b
-; Realizamos una rotación de 6 posiciones a la derecha
-MOV BH, 2
-CALL ROTARDER_N ; AH = 10011100 (C9H)
+ MOV AH, b ; AH = 10000000
+ ; Realizamos una rotación
+ MOV BH,2
+ CALL ROTARDER_N ; AH = 00100000
+ 
 HLT
 END
+
