a5709405eb99718be332a5e88a631495b20ac3ed
Fabo xw <fabianmartinezrincon.123@gmail.com>
2021-09-15 07:11:30 -0300

    comentarios

--- Practicas/Practica_1/8.B.asm
+++ Practicas/Practica_1/8.B.asm
@@ -1,27 +1,28 @@
 ORG 1000H
- CAD DB "ab12355Wsz"
+ CAD DB "ab1sz"
  FIN_CADENA DW 00H
  CANT DW ?
  
 ORG 3000H
- CONTAR_MIN: CMP WORD PTR [BX], 0
- JZ CORTE
+ CONTAR_MIN: CMP WORD PTR [BX], 00H;Mientras no llegue al final
+ JZ CORTE ;Cuando llego al final, termino
  
- MOV AL, [BX]
- CMP AL, 61H
- JS SIG
- CMP AL, 7BH
- JNS SIG
- INC CX
+ MOV AL, [BX] ;Paso el caracter
+ CMP AL, 61H ;Lo comparo con "a"
+ JS SIG ;Si es mas chico que "a", salto
+ CMP AL, 7BH ;Comparo con 1+ que "z"
+ JNS SIG ;Si no es negativo, significa que me pase
+ INC CX ;Si cumple con lo anterior, sumo en mi contador
  
- SIG: INC BX
- JMP CONTAR_MIN
+ SIG: INC BX ;Recorro los caracteres
+ JMP CONTAR_MIN ;Voy al principio y vuelvo a preguntar
  
- CORTE: MOV CANT, CX
- RET
+ CORTE: MOV CANT, CX ;Termino y paso todo a mi variable cant
+ RET ;
 
 ORG 2000h
- MOV BX, OFFSET CAD
+ ;Por referencia via registro
+ MOV BX, OFFSET CAD ;1000h
  CALL CONTAR_MIN
  HLT
 END
