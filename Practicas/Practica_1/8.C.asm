ad04cefeee7f0045d714568a73b6fdcfba977b2e
Fabo xw <fabianmartinezrincon.123@gmail.com>
2021-09-15 07:48:39 -0300

    corrigiendo

--- Practicas/Practica_1/8.C.asm
+++ Practicas/Practica_1/8.C.asm
@@ -1,25 +1,53 @@
-ORG 1000H
-resultado DB ? ; cambiar este valor y ver que queda en la variable resultado
-CHAR DB "E"
 ; Recibe el caracter a verificar por AH
 ; Devuelve el resultado en AL
-ORG 3000H
+ORG 4000H
 ES_VOCAL: MOV AL, 0FFH
- CMP AH, 41H ; A
+ CMP AH, 41H ;A
  JZ FIN
- CMP AH, 45H ; E
+ CMP AH, 45H ;E
  JZ FIN
- CMP AH, 49H ; I
+ CMP AH, 49H ;I
  JZ FIN
- CMP AH, 4FH ; O
+ CMP AH, 4FH ;O
  JZ FIN
- CMP AH, 55H ; U
+ CMP AH, 55H ;U
+ JZ FIN
+ CMP AH, 61H ;a
+ JZ FIN
+ CMP AH, 65H ;e
+ JZ FIN
+ CMP AH, 69H ;i
+ JZ FIN
+ CMP AH, 6FH ;o
+ JZ FIN
+ CMP AH, 75H ;u
  JZ FIN
  MOV AL, 00H
 FIN: RET
+
+ORG 1000H
+CAD DB "EXCELENTe" 
+CERO DB 0
+resultado DW ?
+; Recibe en BX la dirección de la cadena
+; retorna en CX la cantidad de vocales
+
+ORG 3000H
+ VOCALES: MOV CX, 0 ; cantidad de vocales
+ LOOP: MOV AH, BYTE PTR [BX] ; pongo en AX el caracter correspondiente a [BX]
+  CMP AH, 0 ; si llegue al valor 0 (fin de cadena)
+  JZ fin_vocales ; retorno
+  CALL ES_VOCAL ;Si no llegue al final
+  CMP AL, 0FFH ; si no son iguales, no es vocal
+  JNZ NOES
+  INC CX ; incremento vocales
+  NOES: INC BX ; me muevo por la cadena
+   JMP LOOP ; verifico el próximo char
+  fin_vocales: RET
+
 ORG 2000h
-MOV AH, CHAR
-CALL ES_VOCAL
-MOV resultado, AL
-HLT
+ MOV BX, offset CAD
+ CALL VOCALES
+ MOV resultado, CX
+ HLT
 END
