4651de77697385a6814182a11e010614766c824f
fabian-martinez1 <fabianmartinezrincon.123@gmail.com>
2021-09-28 21:27:14 -0500

    ejercicio 4

--- Practicas/Practica_2/4.asm
+++ Practicas/Practica_2/4.asm
@@ -1,20 +1,22 @@
 ORG 1000H
- MAYUSCULAS DB 40H
+ MENSAJE DB "INGRESE UN NUMERO: "
  FIN DB ?
- MINUSCULAS DB 60H
- FIN2 DB ?
- CONT DB 26
+ORG 1500H
+ NUM DB ?
 ORG 2000H ;
- 
- LOOP: INC MAYUSCULAS
- INC MINUSCULAS
- MOV BX, OFFSET MAYUSCULAS
- MOV AL, OFFSET FIN - OFFSET MAYUSCULAS
- INT 7    
- MOV BX, OFFSET MINUSCULAS
- MOV AL, OFFSET FIN2 - OFFSET MINUSCULAS
- INT 7  
- DEC CONT 
- JNZ LOOP
+ MOV BX, OFFSET MENSAJE
+ MOV AL, OFFSET FIN-OFFSET MENSAJE
+ INT 7
+ MOV BX, OFFSET NUM
+ INT 6
+ MOV AL, 1
+ INT 7
+ MOV CL, NUM
  INT 0
+
 END
+;a) en BX almaceno la direccion del primer caracter del mensaje 
+; y en AL indico de que tamaño es la cadena
+;b)  Necesita saber la direccion de la variable que voy a leer
+;c) Ponemos el al en 1 ya que solo vamos a imprimir un solo digito :D
+; Y cl vale 31h que seria el numero ingresado per en hexadecimal
