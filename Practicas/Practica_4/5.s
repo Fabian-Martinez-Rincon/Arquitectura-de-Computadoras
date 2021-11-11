5e17cf0098e0fb8f8aec51934f6de1aea7016dc7
fabian-martinez1 <fabianmartinezrincon.123@gmail.com>
2021-11-11 14:58:59 -0300

    practica 4 ejercicio 6

--- Practicas/Practica_4/5.s
+++ Practicas/Practica_4/5.s
@@ -3,13 +3,13 @@
     datos: .word 1, 2, 3, 4, 5, 6, 7, 8
     res: .word 0
 .code
-    dadd r1, r0, r0
-    ld r2, cant(r0)
-    loop: ld r3, datos(r1)
-        daddi r2, r2, -1
-        dsll r3, r3, 1
-        sd r3, res(r1)
-        daddi r1, r1, 8
-        bnez r2, loop
+    dadd R1, R0, R0         ;INICIALIZO R1
+    ld R2, cant(R0)         ; R2 = 8
+    loop: ld R3, datos(R1)  ; R3 = ELEMENTO DE TABLA EN X POSICION
+        daddi R2, R2, -1    ; DECREMENTO EL CONTADOR DE TABLA
+        dsll R3, R3, 1      ; MUEVO LOS BITS UNA POSICION A LA IZQUIERDA QUE SERIA EL EQUIVALENTE A MULTIPLICARLOS X2
+        sd R3, res(R1)      ; R3 = REMPLAZO EL VALOR NUEVO EN LA TABLA
+        daddi R1, R1, 8     ; ME MUEVO AL SIGUIENTE ELEMENTO DE LA TABLA
+        bnez R2, loop       ; MIENTRAS NO TERMINE DE RECORRER LA TABLA, CONTINUO
     nop
 halt
