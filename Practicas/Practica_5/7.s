3f95ec1f9f163c1b65f35095ea9ef41afe074615
fabian-martinez1 <fabianmartinezrincon.123@gmail.com>
2021-11-13 14:09:38 -0300

    ejercicio 7

--- Practicas/Practica_5/7.s
+++ Practicas/Practica_5/7.s
@@ -1,24 +1,24 @@
 .data
-    m: .word 14
-    tabla: .word 50,12,15,11,2
-    resultado: .word 0
+    M: .word 3
+    TABLA: .word 1,2,3,4,5
+    RESULTADO: .word 0
 .code
-    ld $a0, m(r0)
-    daddi $a1, $0, 5
-    daddi $a2, $0, tabla
-    jal valores
-    nop
-    sd $v0, resultado($0)
+    ld $a0, M(r0)           ; a0 = 14
+    daddi $a1, $0, 5        ; a1 = cantidad de elementos
+    daddi $a2, $0, TABLA    ; a2 = paso la direccion de TABLA
+    jal VALORES             ; salto a la rutina y guardo la direccion de retorno
+    nop                     ; pinto
+    sd $v0, RESULTADO($0)   ; VALORES de retorno de la subrutina, lo guardo en RESULTADO
     halt
 
-valores: dadd $t0, $a1, $0
-         dadd $v0, $0, $0
-loop:     ld $t4, 0($a2)
-         slt $t2, $t4, $a0
-         bnez $t2 ,inc
-         j noinc 
-inc:        daddi $v0, $v0, 1
-noinc:     daddi $a2, $a2, 8
-         daddi $t0, $t0, -1
-         bnez $t0, loop
-         jr $ra
+VALORES: dadd $t0, $a1, $0              ; guardo 5 en el registro temporal
+    dadd $v0, $0, $0                    ; inicializo v0
+    LOOP: ld $t4, 0($a2)                ; copia en el registro temporal el primer elemento de la TABLA
+        slt $t2, $t4, $a0               ; si t4 es menor que a0, entonces dejo 1 en t2
+        bnez $t2 ,NO_INCREMENTAR        ; si t2 <> 0 entonces salto a NO_INCREMENTAR
+        beq $t4, $a0, NO_INCREMENTAR    ; Si es igual, no incremento
+        daddi $v0, $v0, 1               ; aumento el contador 
+    NO_INCREMENTAR: daddi $a2, $a2, 8   ; avanzo en la tabla
+        daddi $t0, $t0, -1              ; decremento el contador de elementos de tabla
+        bnez $t0, LOOP                  ; si t0 <> 0 salto a loop
+        jr $ra                          ; vuelve al programa principal
